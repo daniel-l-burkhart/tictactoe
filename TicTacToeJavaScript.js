33e80ff44b5f7b129774b980712599021cd656f1
dburkha1 <dburkha1@my.westga.edu>
2015-01-23 17:31:38 -0500

    Did some formatting and fixed some bugs. Used the bitwise &. Simpler spot checking as well.

--- TicTacToeJavaScript.js
+++ TicTacToeJavaScript.js
@@ -5,7 +5,11 @@ var player1 = [];
 
 var player2 = [];
 
-var usedSpots = [];
+//var usedSpots = [];
+
+var moves = 0;
+
+var hasBeenDrawn = false;
 
 
 var winningScores = [7, 56, 448, 273, 84, 73, 146, 292];
@@ -19,29 +23,32 @@ function canvasClicked(power) {
     ctx.font = "20px Georgia";
 
 
-    if (spotIsTaken(power)) {
+    if (ctx.hasBeenDrawn === true) {
         alert("Spot Taken");
     
     } else {
 
-        if (player1.length === player2.length) {
+        if (moves % 2 === 0) {
+            moves += 1;
             player1[player1.length] = numericVal;
             ctx.fillText("X", 20, 30, 50);
+            ctx.hasBeenDrawn = true;
             checkForWin(player1, "X");
+            
 
         } else {
+            moves += 1;
             player2[player2.length] = numericVal;
             ctx.fillText("O", 20, 30, 50);
-            checkForWin(player2, "O");
+            ctx.hasBeenDrawn = true;
+            checkForWin(player2, "O");  
         }
-        usedSpots[usedSpots.length] = power;
-      
-       
+        //usedSpots[usedSpots.length] = power;   
     }
     
 }
 
-function spotIsTaken(index) {
+/*function spotIsTaken(index) {
     var isTaken = false;
     var i = 0;
     usedSpots.forEach(function(spot) {
@@ -52,18 +59,29 @@ function spotIsTaken(index) {
     });
 
     return isTaken;
-}
+}*/
 
 function checkForWin(player, playerName) {
-    var i = 0;
-   while (i < winningScores.length)
+
+    for (var i = 0; i < winningScores.length; i += 1) {
+        if ((winningScores[i] & sum(player)) === winningScores[i]) {
+            alert("player " + playerName + " wins!");
+            startNewGame();
+
+        } else if(moves === 9){
+            alert("The game is a draw. No one wins.");
+            startNewGame();
+        }
+    }
+    
+   /*while (i < winningScores.length)
     {
       if (parseInt(sum(player)) === parseInt(winningScores[i])) {
           alert("player " + playerName + " wins!");
           location.reload();
       }
         i++;
-    }
+    }*/
     
 }
 
@@ -79,6 +97,19 @@ function sum(list) {
     return total;
 
 }
+
+function startNewGame() {
+    var canvas = document.getElementsByTagName("canvas");
+    for (var i =0; i<canvas.length; i+=1) {
+        var context = canvas[i].getContext("2d");
+        context.clearRect(0, 0, canvas[i].width, canvas[i].height);
+        context.hasBeenDrawn = false;
+        context.restore();
+    }
+    player1 = [];
+    player2 = [];
+    moves = 0;
+} 
 
 
 
