a574cc065fdbd29b6140b23fc12a3861e7799011
baxleybr <lbaxley57@yahoo.com>
2015-01-23 21:37:46 -0800

    Fixed bugs in JavaScript. Last iteration would say nobody won and then whoever won right after (if somebody won).

--- TicTacToeJavaScript.js
+++ TicTacToeJavaScript.js
@@ -5,11 +5,9 @@ var player1 = [];
 
 var player2 = [];
 
-//var usedSpots = [];
 
 var moves = 0;
 
-var hasBeenDrawn = false;
 
 
 var winningScores = [7, 56, 448, 273, 84, 73, 146, 292];
@@ -22,68 +20,62 @@ function canvasClicked(power) {
     var ctx = canvas[power].getContext("2d");
     ctx.font = "20px Georgia";
 
-
+    
+    
     if (ctx.hasBeenDrawn === true) {
         alert("Spot Taken");
     
     } else {
 
         if (moves % 2 === 0) {
-            moves += 1;
+            setMove();
             player1[player1.length] = numericVal;
             ctx.fillText("X", 20, 30, 50);
             ctx.hasBeenDrawn = true;
             checkForWin(player1, "X");
             
+          
 
         } else {
-            moves += 1;
+            setMove();
             player2[player2.length] = numericVal;
             ctx.fillText("O", 20, 30, 50);
             ctx.hasBeenDrawn = true;
             checkForWin(player2, "O");  
+            
+          
         }
-        //usedSpots[usedSpots.length] = power;   
+         
     }
-    
+ checkForCatGame();
+
 }
 
-/*function spotIsTaken(index) {
-    var isTaken = false;
-    var i = 0;
-    usedSpots.forEach(function(spot) {
-        if (parseInt(spot) === parseInt(index)) {
-            isTaken = true;
-        }
-        i++;
-    });
 
-    return isTaken;
-}*/
 
 function checkForWin(player, playerName) {
+
 
     for (var i = 0; i < winningScores.length; i += 1) {
         if ((winningScores[i] & sum(player)) === winningScores[i]) {
             alert("player " + playerName + " wins!");
             startNewGame();
 
-        } else if(moves === 9){
-            alert("The game is a draw. No one wins.");
-            startNewGame();
         }
-    }
     
-   /*while (i < winningScores.length)
-    {
-      if (parseInt(sum(player)) === parseInt(winningScores[i])) {
-          alert("player " + playerName + " wins!");
-          location.reload();
-      }
-        i++;
-    }*/
-    
+
 }
+
+}    
+
+
+function checkForCatGame() {
+    if(moves === 9){
+            alert("The game is a draw. No one wins.");
+        startNewGame();
+       
+    }
+    }
 
 function sum(list) {
     var total = 0;
@@ -109,7 +101,15 @@ function startNewGame() {
     player1 = [];
     player2 = [];
     moves = 0;
+    
+  
 } 
+
+function setMove() {
+ 
+        moves += 1;
+    
+}
 
 
 
