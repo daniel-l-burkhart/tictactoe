4e41bd03ea02d2ede38d741af821d513fa671090
unknown <csuser@CS-TLC1117-W16.ad.cs.westga.edu>
2015-02-03 20:23:27 -0500

    finished first 3 tests

--- js/Game.js
+++ js/Game.js
@@ -5,7 +5,8 @@ function Game() {
 	this.oPlayer.score = 0;
 	this.board = new Board();
 	this.board.setSpaces();
-
+	this.currentPlayer = this.xPlayer;
+	
 	this.newGame = function() {
 		this.board = new Board();
 		this.board.setSpaces();
@@ -13,46 +14,39 @@ function Game() {
 		this.oPlayer = new Player("O");
 		this.xPlayer.score = 0;
 		this.oPlayer.score = 0;
-	
+		this.currentPlayer = this.xPlayer;
 		location.reload();
 	};
 	
+    this.switchCurrentPlayers = function() {
+		if(this.currentPlayer === this.xPlayer) {
+			this.currentPlayer = this.oPlayer;
+		} else {
+			this.currentPlayer = this.xPlayer;
+		}
+	}
+	this.movePlayer = function(currPlayer, index) {
+		currPlayer.score += this.board.spaces[index].value;
+		this.board.spaces[index].playerMarked = currPlayer.Name;
+			if (checkForWinner(currPlayer.score)) {
+				alert("Player " + currPlayer.Name + " wins");
+				this.newGame();
+				}
+			currPlayer.turn +=1;
+	}		
 	this.makeMove = function(index) {
 		if(this.board.spaces[index].Marked === false){
-		if(this.xPlayer.turn === this.oPlayer.turn) {
-			this.xPlayer.score += this.board.spaces[index].value;
-			
-			this.board.spaces[index].playerMarked = "X";
-
-			if (checkForWinner(this.xPlayer.score)) {
-				alert("Player X wins");
+			this.movePlayer(this.currentPlayer, index);
+			this.switchCurrentPlayers();
+			this.board.spaces[index].Marked = true;
+			if (this.board.isBoardFilledIn()) {
+				alert("The Game is a Draw.");
 				this.newGame();
-			}
-			this.xPlayer.turn+=1;
-		} else {
+			}	
 			
-			this.oPlayer.score += this.board.spaces[index].value;
-			this.board.spaces[index].playerMarked = "O";
-
-			if (checkForWinner(this.oPlayer.score)) {
-				alert("Player O wins");
-				this.newGame();
-			}
-			this.oPlayer.turn+=1;
+		} else{
+			alert("Space is taken.");
 		}
-		
-		
-		this.board.spaces[index].Marked = true;
-		if (this.board.isBoardFilledIn()) {
-			
-				alert("Cat Game");
-				this.newGame();
-	
-		
-		}
-	} else{
-		alert("Space is taken.");
-	}
 	};
 
 }
--- tests/spec/tictactoespec.js
+++ tests/spec/tictactoespec.js
@@ -1,95 +1,87 @@
-
 describe("The values of cells are correct", function() {
+		var currentGame = new Game();
+		currentGame.board.setSpaces();
 	it("should return 1 for the first cell.", function() {
-		expect(getNumericVal(0)).toBe(1); 
-		
+		expect(currentGame.board.spaces[0].value).toBe(1); 
+	});
+	it("should return 2 for the second cell.", function() {
+		expect(currentGame.board.spaces[1].value).toBe(2); 
+	});
+	it("should return 4 for the third cell.", function() {
+		expect(currentGame.board.spaces[2].value).toBe(4); 
+	});
+	it("should return 8 for the fourth cell.", function() {
+		expect(currentGame.board.spaces[3].value).toBe(8); 
+	});
+	it("should return 16 for the fifth cell.", function() {
+		expect(currentGame.board.spaces[4].value).toBe(16); 
+	});
+	it("should return 32 for the sixth cell.", function() {
+		expect(currentGame.board.spaces[5].value).toBe(32); 
+	});
+	it("should return 64 for the seventh cell.", function() {
+		expect(currentGame.board.spaces[6].value).toBe(64); 
+	});
+	it("should return 128 for the eighth cell.", function() {
+		expect(currentGame.board.spaces[7].value).toBe(128); 
+	});
+		it("should return 256 for the ninth cell.", function() {
+		expect(currentGame.board.spaces[8].value).toBe(256); 
 	});
 });
 
-describe("The first move should return X", function() {
-	it("X will be the player on the 0th turn (first).", function() {
-			expect(getPlayer(0)).toBe("X"); 
-		
+describe("Player Changes after each turn.", function() {
+	var currentGame = new Game();
+	currentGame.board.setSpaces();
+	
+	it("Player X should be the first current Player.", function() {
+		expect(currentGame.currentPlayer.Name).toBe("X"); 
 	});
+	
+	it("Player O should be the Second current Player.", function() {
+		currentGame.makeMove(1);
+		expect(currentGame.currentPlayer.Name).toBe("O"); 
+	});
+
 });
 
-describe("The second move should return O", function() {
-	it("O will be the player on the 1st turn (second).", function() {
-			expect(getPlayer(1)).toBe("O"); 
-		
-	});
-});
 
-describe("The second move should return O", function() {
-	it("X will be the player on the 2nd turn (third).", function() {
-			expect(getPlayer(2)).toBe("X"); 
-		
+describe("Player Score Changes after each turn.", function() {
+	
+	it("Start: Player X Score = 0,  Player O Score = 0.", function() {
+		var currentGame2 = new Game();
+		currentGame2.board.setSpaces();
+		expect(currentGame2.xPlayer.score).toBe(0); 
+		expect(currentGame2.oPlayer.score).toBe(0); 
 	});
-});
-
-describe("The second move should return O", function() {
-	it("O will be the player on the 3rd turn (fourth).", function() {
-			expect(getPlayer(3)).toBe("O"); 
-		
-	});
-});
-
-describe("This should return a winning score.", function() {
-	it("Player 2 should win going across", function() {
-			player2[player2.length] = getNumericVal(0);
-			player2[player2.length] = getNumericVal(1);
-			player2[player2.length] = getNumericVal(2);
-			
-			expect(checkForWin(player2, "O")).toBe(true); 
-		
-	});
-});
-
-describe("This should return a winning score.", function() {
-	it("Player 1 should win going across", function() {
-			player1[player1.length] = getNumericVal(0);
-			player1[player1.length] = getNumericVal(1);
-			player1[player1.length] = getNumericVal(2);
-			
-			expect(checkForWin(player1, "X")).toBe(true); 
-		
-	});
-});
-
-describe("This should return a winning score.", function() {
-	it("Player 2 should win going down", function() {
-			player2 = [];
-			player2[player2.length] = getNumericVal(0);
-			player2[player2.length] = getNumericVal(3);
-			player2[player2.length] = getNumericVal(6);
-			
-			expect(checkForWin(player2, "O")).toBe(true); 
-		
-	});
-});
-
-describe("This should return a winning score.", function() {
-	it("Player 1 should win going down", function() {
-			player1 = [];
-			player1[player1.length] = getNumericVal(0);
-			player1[player1.length] = getNumericVal(3);
-			player1[player1.length] = getNumericVal(6);
-			
-			expect(checkForWin(player1, "O")).toBe(true); 
-		
-	});
-});
-
-describe("Checking for adding correct numbers to player lists.", function() {
-	it("Should have 7 in player 1.", function() {
-		player1 = [];
-			player1[player1.length] = getNumericVal(0);
-			player1[player1.length] = getNumericVal(1);
-			player1[player1.length] = getNumericVal(2);
-			
-			expect(sum(player1)).toBe(7); 
-		
-	}); 
-}); 
 	
 	
+	it("After Turn 1: Player X Score = 1, Player O Score = 0.", function() {
+		var currentGame2 = new Game();
+		currentGame2.board.setSpaces();
+		currentGame2.makeMove(0);
+		expect(currentGame2.xPlayer.score).toBe(1); 
+		expect(currentGame2.oPlayer.score).toBe(0);
+	});
+	
+	it("After Turn 2: Player X Score = 1, Player O Score = 4.", function() {
+		var currentGame2 = new Game();
+		currentGame2.board.setSpaces();
+		currentGame2.makeMove(0);
+		currentGame2.makeMove(2);
+		expect(currentGame2.xPlayer.score).toBe(1); 
+		expect(currentGame2.oPlayer.score).toBe(4);
+	});
+	
+	it("After Turn 3: Player X Score = 3, Player O Score = 4.", function() {
+		var currentGame2 = new Game();
+		currentGame2.board.setSpaces();
+		currentGame2.makeMove(0);
+		currentGame2.makeMove(2);
+		currentGame2.makeMove(1)
+		expect(currentGame2.xPlayer.score).toBe(3); 
+		expect(currentGame2.oPlayer.score).toBe(4);
+	});
+});
+	
+
